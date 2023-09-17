7dafc4f6a4bc1d0c152a0752daf65efd97ef1a7c
yessetassan <139701904+yessetassan@users.noreply.github.com>
2023-09-18 00:08:16 +0600

    Empty Project

---
+++ student_and_teacher/src/main/java/com/example/student_and_teacher/StudentAndTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_and_teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAndTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAndTeacherApplication.class, args);
+	}
+
+}
---
+++ student_and_teacher/src/test/java/com/example/student_and_teacher/StudentAndTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_and_teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAndTeacherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
